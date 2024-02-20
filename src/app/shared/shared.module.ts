2b3f739aa8b657c6640dfac6e0a8898cbb9b4916
Josiel Castillo <josielcastillov@gmail.com>
2024-02-20 16:31:48 -0600

    Capitulo 115

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomepageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomepageComponent,
+    AboutPageComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HomepageComponent,
+    AboutPageComponent
   ]
 })
 export class SharedModule { }
